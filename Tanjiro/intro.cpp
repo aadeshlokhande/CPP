bb1852870c18bb127cf19ed2da4a3684a2a46ab8
Aadesh <aadeshlokhande11@gmail.com>
2024-07-13 17:04:46 +0530

    13 / 07 / 2024

---
+++ Tanjiro/intro.cpp
@@ -0,0 +1,47 @@
+// 1972 ---> denish ritchy ----> c lang 
+// bell lab 
+
+// stdio.h 
+
+// iostream
+
+
+// class and object 
+// c with classes 
+
+
+// OOPs 
+// class 
+// objects 
+// inheritance
+// polymorphism
+// encapsulation
+// abstraction 
+
+// friend function 
+// constructor 
+// distructor 
+// .
+// .
+// .
+// .
+// a = 10 
+// a++
+
+// c++
+
+#include<iostream>
+// using namespace std;
+
+int main()
+{
+    // int a;
+    // std :: cout << "Enter a number = ";
+    // std :: cin >> a;
+
+    std :: cout << "adesh " << 90 << std :: endl;
+    std :: cout << "adesh " << 90 << "\n";
+    
+    return 0;
+}
+
